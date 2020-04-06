16db37adae10644b32b2f989be72e79c199683cd
alkarps <alserkarpov@gmail.com>
2020-04-06 13:07:05 +0300

    Добавлено описание + сбор статистики операций

--- hw01-gradle/src/main/java/my/alkarps/HelloOtus.java
+++ hw01-gradle/src/main/java/my/alkarps/HelloOtus.java
@@ -4,6 +4,7 @@ import com.google.common.collect.ContiguousSet;
 import com.google.common.math.IntMath;
 
 import java.math.RoundingMode;
+import java.util.IntSummaryStatistics;
 import java.util.stream.Collectors;
 
 import static com.google.common.collect.DiscreteDomain.integers;
@@ -11,15 +12,14 @@ import static com.google.common.collect.Range.closed;
 
 public class HelloOtus {
     public static void main(String... args) {
-        String result = ContiguousSet.create(closed(1, 100), integers())
+        IntSummaryStatistics result = ContiguousSet.create(closed(1, 100), integers())
                 .stream()
-                .map(HelloOtus::map)
-                .map(Object::toString)
-                .collect(Collectors.joining(", "));
+                .map(HelloOtus::doSomething)
+                .collect(Collectors.summarizingInt(value -> value));
         System.out.println(result);
     }
 
-    private static Integer map(Integer value) {
+    private static Integer doSomething(Integer value) {
         if (IntMath.mod(value, 2) == 0) {
             return IntMath.log2(value, RoundingMode.HALF_EVEN);
         } else {
