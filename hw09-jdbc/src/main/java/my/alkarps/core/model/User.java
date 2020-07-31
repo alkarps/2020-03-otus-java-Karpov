b76e20dda4e4aa22d6efa3153c5b3a6a5d99e6cb
alkarps <alserkarpov@gmail.com>
2020-07-31 13:29:24 +0300

    расширение юзера

--- hw09-jdbc/src/main/java/my/alkarps/core/model/User.java
+++ hw09-jdbc/src/main/java/my/alkarps/core/model/User.java
@@ -1,12 +1,20 @@
 package my.alkarps.core.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import my.alkarps.jdbc.mapper.annotation.Id;
+
 /**
  * @author sergey
  * created on 03.02.19.
  */
+@Getter
 public class User {
+    @Id
     private final long id;
     private final String name;
+    @Setter
+    private int age;
 
     public User(long id, String name) {
         this.id = id;
